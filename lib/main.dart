79f74e5fb63a407a9fe060416424af18d48617f3
Slipcod <Slipcod@gmail.com>
2019-09-17 22:24:17 +0300

    Interface sketch has been created.

--- lib/main.dart
+++ lib/main.dart
@@ -44,71 +44,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Testing merging in github',
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child:Row(
+              children: <Widget>[
+                _buildBox(flex: 8, letter: "Display", color: Colors.black38),
+              ],
             ),
-            Text(
-              'You have pushed the button this many times:',
+          ),
+          Expanded(
+            flex: 1,
+            child:Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: <Widget>[
+                      _buildBox(flex: 8, letter: "Numbers", color: Colors.black12),
+
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: <Widget>[
+                      _buildBox(flex: 8, letter: "signs", color: Colors.brown),
+
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )
+        ],
+      )
     );
   }
+
+
+
+
+
+  Widget _buildBox({int flex,String letter,  Color color, Color textColor = Colors.white}) {
+    return Expanded(
+      flex: flex,
+      child: Container(
+        constraints: BoxConstraints.expand(),
+        color: color,
+        child: Center(
+          child: Text(
+            '$letter',
+            style: TextStyle(fontSize: 32.0, color: textColor),
+          ),
+        ),
+      ),
+    );
+  }
+
 }
